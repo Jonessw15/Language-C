b8a16b53231f626cfdfbae6a1972ba18679155b1
jhonatan <jhonatanalves6860@gmail.com>
2023-07-09 19:03:31 -0300

    Subino

--- C/Trabalho/Ifood.c
+++ C/Trabalho/Ifood.c
@@ -36,8 +36,10 @@ int cadastrarClientes();
 /* VARIAVEIS GLOBAIS */
 int tamanho = 1000;
 Clientes list[1000];
+Ifood listProd[1000];
 int quantidade = 0;
 int caso = 0;
+int quantidadeProdutos = 0;
 
 int main() {
 
@@ -63,6 +65,9 @@ int main() {
 
     /* INÍCIO DO PROJETO NA FUNÇÃO PRINCIPAL */
     perguntas();
+    while (caso != 11) {
+        selecionarFuncao(caso);
+    }
     printf("Obrigado pela visita");
 
     fclose(arquivo);
@@ -71,13 +76,38 @@ int main() {
 }
 
 void perguntas() {
-    while (caso != 11) {
+    while (caso > 0 || caso <= 11) {
+        while (caso > 0 || caso <= 11) {
+            printf("Bem vindo, meu consagrado\n\n");
+            printf("1) Cadastro de Produtos\n");
+            printf("2) Listar Produtos\n");
+            printf("3) Cadastro de Clientes\n");
+            printf("4) Listar Clientes\n");
+            printf("5) Realiza Pedidos\n");
+            printf("11) Sair do Programa\n\n");
+            printf("Escolha: ");
+            scanf("%d", &caso);
 
+            system("cls");
+
+            if (caso > 0 || caso <= 11) {
+                break;
+            }
+        }
+        if (caso > 0 || caso <= 11) {
+            break;
+        }
     }
 }
 
 void selecionarFuncao(int opcao) {
-    if (opcao == 3) {
+    if (opcao == 1) {
+        printf("\nCadastro de produtos\n\n");
+        cadastrarProdutos();
+    } else if (opcao == 2) {
+        printf("\nLista de produtos\n\n");
+        listaProdutos();
+    } else if (opcao == 3) {
         printf("\nCadastro de clientes\n\n");
         cadastrarClientes();
     } else if (opcao == 4) {
@@ -85,6 +115,45 @@ void selecionarFuncao(int opcao) {
     }
     caso = 0;
     perguntas();
+}
+
+//FUNÇÃO DE CADASTRAR PRODUTOS QUE O CLAITON FEZ
+int cadastrarProdutos(){
+    if (quantidadeProdutos < tamanho) {
+        Ifood produtos;
+        getchar();
+        printf("Digite o nome do produto: ");
+        fgets(produtos.nomeProduto, 30, stdin);
+
+        produtos.nomeProduto[strcspn(produtos.nomeProduto, "\n")] = '\0';
+
+        printf("Digite a descricao do produto: ");
+        fgets(produtos.descricao, 50, stdin);
+
+        produtos.descricao[strcspn(produtos.descricao, "\n")] = '\0';
+
+
+        printf("Digite o preco do produto: ");
+        scanf("%f", &produtos.preco);
+
+        getchar();
+        listProd[quantidade] = produtos;
+        quantidadeProdutos++;
+        return 1;
+    } else {
+        printf("O sistema alcancou o limite maximo.\nPara contratar mais servicos ligue 4002-8922");
+        return 0;
+    }
+}
+//LISTAR PRODUTOS QUE O CLAITON FEZ
+void listaProdutos(){
+    for (int i = 0; i < quantidade; i++) {
+        printf("\nID do Produto: %d\n", listProd[i].ID);
+        printf("Nome do produto: %s\n", listProd[i].nomeProduto);
+        printf("Descricao do produto: %s\n", listProd[i].descricao);
+        printf("Preco do produto: %2.f\n", listProd[i].preco);
+    }
+    printf("\n");
 }
 
 int cadastrarClientes() {
@@ -102,13 +171,19 @@ int cadastrarClientes() {
         customers.nome[strcspn(customers.nome, "\n")] = '\0';
 
         printf("Digite o seu endereco: ");
-        fgets(customers.email, 50, stdin);
+        fgets(customers.endereco, 50, stdin);
 
+        customers.endereco[strcspn(customers.endereco, "\n")] = '\0';
+
+        printf("Digite seu e-mail: ");
+        fgets(customers.email, 100, stdin);
+
+        customers.email[strcspn(customers.email, "\n")] = '\0';
 
         printf("Digite seu numero de telefone: ");
         fgets(customers.telefone, 30, stdin);
 
-        getchar();
+        customers.telefone[strcspn(customers.telefone, "\n")] = '\0';
 
         list[quantidade] = customers;
 
